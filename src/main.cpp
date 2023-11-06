1e6d73d05a57b632020efc741185b010da5b22e8
Kyle <kcparks1234@gmail.com>
2023-11-06 12:14:24 -0800

    Created opening screen and transition into main game. Current background is a placeholder

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
 #include <SFML/Graphics.hpp>
 
+void startGame() {
+}
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My Game");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My Game - Opening Screen");
+
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("assets/background.jpg")) {
+    }
+    sf::Sprite background(backgroundTexture);
+
+    sf::Font font;
+    if (!font.loadFromFile("assets/Roboto-Bold.ttf")) {
+    }
+    sf::Text title("My Game", font, 50);
+    title.setPosition(200, 100);
+
+    sf::Text prompt("Press Enter to start", font, 20);
+    prompt.setPosition(200, 300);
+
+    bool gameStarted = false;
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter) {
+                gameStarted = true;
+                // Start the game or transition to the main game loop
+            }
         }
 
         window.clear();
-        // Draw your game contents here
+        if (!gameStarted) {
+            // If game hasn't started, show the opening screen
+            window.draw(background);
+            window.draw(title);
+            window.draw(prompt);
+        } else {
+            // The game has started, run the game loop
+            startGame();  
+        }
         window.display();
     }
 
